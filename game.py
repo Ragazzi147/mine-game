d1957ab33efbe5510b08b53bfcd46bed2b0f8ce5
Lucas Ragazzi Pereira Maciel <106124@sequoia.local>
2023-04-03 09:10:09 -0300

    new texture

--- game.py
+++ game.py
@@ -1,22 +1,35 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
+app = Ursina()
+grass_texture = load_texture('assets/grass_block.png')
+stone_texture = load_texture('assets/stone_block.png')
+brick_texture = load_texture('assets/brick_block.png')
+dirt_texture  = load_texture('assets/dirt_block.png')
+
 class Voxel(Button):
     def __init__(self, position = (0,0,0)):
         super().__init__(
             parent = scene,
             position = position,
-            model = 'cube',
+            model = 'assets/block',
             origin_y = 0.5,
-            texture = 'white_cube',
-            color = color.white,
-            highlight_color = color.lime
+            texture = grass_texture,
+            color = color.color(0,0,random.uniform(0.9,1)),
+            highlight_color = color.lime,
+            scale = 0.5,
         ) 
+    def input(self,key):
+        if self.hovered:
+            if key == 'left mouse down':
+                voxel = Voxel(position = self.position + mouse.normal)
+            if key == 'right mouse down':
+                destroy(self)
 
-app = Ursina()
 
-for z in range(8):
-    for x in range (8):
-        voxel = Voxel((x,0,z))
+for z in range(20):
+    for x in range (20):
+        
+            voxel = Voxel((x,0,z))
 player = FirstPersonController()
 app.run()
