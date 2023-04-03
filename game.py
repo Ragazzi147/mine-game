09d5420845b3a4599ec5391c7ec75aca0c91191d
Lucas Ragazzi Pereira Maciel <106124@sequoia.local>
2023-04-03 09:43:56 -0300

    repare texture

--- game.py
+++ game.py
@@ -6,23 +6,32 @@ grass_texture = load_texture('assets/grass_block.png')
 stone_texture = load_texture('assets/stone_block.png')
 brick_texture = load_texture('assets/brick_block.png')
 dirt_texture  = load_texture('assets/dirt_block.png')
+block_pick = 1
+
+def update():
+     global block_pick
+     if held_keys['1']: block_pick = 1
+     if held_keys['2']: block_pick = 2
+     if held_keys['3']: block_pick = 3
+     if held_keys['4']: block_pick = 4
+
 
 class Voxel(Button):
-    def __init__(self, position = (0,0,0)):
+    def __init__(self, position = (0,0,0), texture = grass_texture):
         super().__init__(
             parent = scene,
             position = position,
             model = 'assets/block',
             origin_y = 0.5,
-            texture = grass_texture,
+            texture = texture,
             color = color.color(0,0,random.uniform(0.9,1)),
-            highlight_color = color.lime,
             scale = 0.5,
         ) 
+
     def input(self,key):
         if self.hovered:
             if key == 'left mouse down':
-                voxel = Voxel(position = self.position + mouse.normal)
+                voxel = Voxel(position = self.position + mouse.normal, texture= stone_texture)
             if key == 'right mouse down':
                 destroy(self)
 
